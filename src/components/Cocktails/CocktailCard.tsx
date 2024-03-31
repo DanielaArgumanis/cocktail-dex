62f88de50eb49b9558fe62dd130e8770bb21a871
DanielaArgumanis <daniela.argumanis@gmail.com>
2024-03-31 11:27:59 -0500

    💥 NEW: Create modal

--- src/components/Cocktails/CocktailCard.tsx
+++ src/components/Cocktails/CocktailCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 
 // @Components
 import Chip from "@theme/Chip";
@@ -8,30 +8,31 @@ import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
 
 // @Types
 import { LiquorColors } from "@theme/Theme.types";
+import CocktailModal from "./CocktailModal";
+import { CocktailCardProps } from "./Cocktail.types";
 
-type CocktailCardProps = {
-    name: string;
-    liquorList: string[];
-    svg: React.ReactNode;
-}
 
-const CocktailCard = ({name, liquorList, svg}: CocktailCardProps) => {
+const CocktailCard = (props: CocktailCardProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const {name, liquorList, svg: SvgIcon} = props;
+    
     return  <Grid item lg={3} md={4} sm={6} xs={12} sx={{display: 'flex', justifyContent: 'center', position:'relative',  marginTop: '50px', '& svg': {
         position: 'absolute',
         top: '-40px'
     }}}>
-    {svg}
-        <Card variant="outlined" sx={{borderRadius: '12px', width: '100%', 
+    <SvgIcon height={100} width={100}/>
+        <Card onClick={()=>setIsOpen(true)} variant="outlined" sx={{borderRadius: '12px', width: '100%', 
     boxShadow: 'rgba(0,0,0,0.25) 0 10px 10px',}}> 
-    <CardContent sx={{textAlign: 'center'}}>
-        <Typography marginTop={8}>{name}</Typography>
-        <Box sx={{display: 'flex', gap: 1, justifyContent: 'center', marginY: 2}}>
-            {
-                liquorList.map((liquor)=> <Chip key={liquor} label={liquor} chipColor={liquor.toLowerCase() as keyof LiquorColors}/>)
-            }
-        </Box>
-    </CardContent>
-    </Card>
+        <CardContent sx={{textAlign: 'center'}}>
+            <Typography marginTop={8} variant="h6" fontWeight='bold'>{name}</Typography>
+            <Box sx={{display: 'flex', gap: 1, justifyContent: 'center', marginY: 2}}>
+                {
+                    liquorList.map((liquor)=> <Chip key={liquor} label={liquor} chipColor={liquor.toLowerCase() as keyof LiquorColors}/>)
+                }
+            </Box>
+        </CardContent>
+        </Card>
+        <CocktailModal {...props} isOpen={isOpen} setIsOpen={setIsOpen}/>
     </Grid>
 }
 
